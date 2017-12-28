625d6a26627cb0f693063b204a2014e5039671ef
tett23 <tett23@gmail.com>
2017-12-28 11:36:18 +0900

    e.Startで処理がブロックしてた

--- main.go
+++ main.go
@@ -18,20 +18,23 @@ func main() {
 		job.ProcessJobQueue()
 	}()
 
-	server.Start("localhost", 1323)
-
 	for _, nCode := range conf.NCodes {
 		job.Enqueue(job.JobTypeFetchLatestEpisode, nCode, -1)
 	}
 
-	ch := time.Tick(1 * time.Hour)
-	// ch := time.Tick(5 * time.Second)
-	for {
-		select {
-		case <-ch:
-			for _, nCode := range conf.NCodes {
-				job.Enqueue(job.JobTypeFetchLatestEpisode, nCode, -1)
+	go func() {
+		ch := time.Tick(1 * time.Hour)
+		// ch := time.Tick(5 * time.Second)
+
+		for {
+			select {
+			case <-ch:
+				for _, nCode := range conf.NCodes {
+					job.Enqueue(job.JobTypeFetchLatestEpisode, nCode, -1)
+				}
 			}
 		}
-	}
+	}()
+
+	server.Start("localhost", 1323)
 }
